85218faf409b0653d046b5a0f8466290d57cce2f
yasminhana <92916680+yasminhana@users.noreply.github.com>
2022-06-29 21:15:56 +0800

    Update thirdpage.dart

--- thirdpage.dart
+++ thirdpage.dart
@@ -8,4 +8,28 @@ class ThirdPage extends StatefulWidget {
   State<ThirdPage> createState() => _ThirdPageState();
 }
 
-class _ThirdPageState extends State<ThirdPage> {}
+class _ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TUTOR LIST'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Text('Name: \n Subjects offered: \n Contact number: '),
+                color: Colors.blue,
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(20.0),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
