1c5f1f6ebc44cf837a82151fbf9fbf2d7afb8c21
yasminhana <92916680+yasminhana@users.noreply.github.com>
2022-06-30 20:36:21 +0800

    Update fourthpage.dart

--- fourthpage.dart
+++ fourthpage.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
 import 'routes.dart';
-import 'tutor.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    // Replace with actual values
-    options: FirebaseOptions(
-      apiKey: "",
-      appId: "1:151973943972:android:a023969e53e331e33be431",
-      messagingSenderId: "151973943972",
-      projectId: "bmi1-cb5f1",
-    ),
-  );
-  runApp(MyApp());
-}
 class FourthPage extends StatefulWidget {
   const FourthPage({Key? key}) : super(key: key);
 
@@ -26,7 +11,9 @@ class FourthPage extends StatefulWidget {
 }
 
 class _FourthPageState extends State<FourthPage> {
-   
+  final _formKey = GlobalKey<FormState>();
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   var fullname;
   var icno;
   var contactno;
@@ -37,7 +24,7 @@ class _FourthPageState extends State<FourthPage> {
   void _setfullname(String text) {
     setState(() {
       fullname = text;
-      id = fullname;
+      // id = fullname;
     });
   }
 
@@ -65,18 +52,17 @@ class _FourthPageState extends State<FourthPage> {
     });
   }
 
-  void _create() async{
+  void _create() async {
     try {
-      await firestore.collection('name').doc(id).set({
+      await firestore.collection('tutor').doc(fullname).set({
         'fullname': fullname,
         'icno': icno,
         'contactno': contactno,
         'email': email,
-        'subject' : subject,
+        'subject': subject,
       });
-    }
-    catch(e){
-      print (e);
+    } catch (e) {
+      print(e);
     }
   }
 
@@ -100,123 +86,117 @@ class _FourthPageState extends State<FourthPage> {
     );
   }
 
-  Widget build(BuildContext context) { 
-    return Form(  
-      key: _formKey,  
-      child: Column(  
-        crossAxisAlignment: CrossAxisAlignment.start,  
-        children: <Widget>[ 
-          new Text("Application form to become a tutor", textAlign: TextAlign.center),
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Text("Application form to become a tutor",
+              textAlign: TextAlign.center),
           Padding(
-            padding: EdgeInsets.all(8),
-            child:TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Full name',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '';
-                    }
-                    return null;
-                  }, 
-                  onChanged: (text) {
-                    _setfullname(text);
-                  },
-                  keyboardType: TextInputType.number,
-                )),
-
-          Padding(  
-             padding : EdgeInsets.all(8),
-             child: TextFormField(
-                    decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'IC number',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '';
-                    }
-                    return null;
-                  }, 
-                  onChanged: (text) {
-                    _seticno(text);
-                  },
-                  keyboardType: TextInputType.number,
-                )), 
-
-                Padding(
-            padding: EdgeInsets.all(8),
-            child:TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Contact number',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '';
-                    }
-                    return null;
-                  }, 
-                  onChanged: (text) {
-                    _setcontactno(text);
-                  },
-                  keyboardType: TextInputType.number,
-                )),
-
-                Padding(
-            padding: EdgeInsets.all(8),
-            child:TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '';
-                    }
-                    return null;
-                  }, 
-                  onChanged: (text) {
-                    _setemail(text);
-                  },
-                  keyboardType: TextInputType.number,
-                )),
-
-                Padding(
-            padding: EdgeInsets.all(8),
-            child:TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Subject offered',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '';
-                    }
-                    return null;
-                  }, 
-                  onChanged: (text) {
-                    _setsubject(text);
-                  },
-                  keyboardType: TextInputType.number,
-                )),
-
-            Padding(
-            padding: const EdgeInsets.all(8),
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  _create();
-                }
-              },
-              child: const Text('Submit'),
-              )
-            ) , 
-        ], 
-      ),  
-    );  
-  }  
-}  
+              padding: EdgeInsets.all(8),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Full name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '';
+                  }
+                  return null;
+                },
+                onChanged: (text) {
+                  _setfullname(text);
+                },
+                keyboardType: TextInputType.number,
+              )),
+          Padding(
+              padding: EdgeInsets.all(8),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'IC number',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '';
+                  }
+                  return null;
+                },
+                onChanged: (text) {
+                  _seticno(text);
+                },
+                keyboardType: TextInputType.number,
+              )),
+          Padding(
+              padding: EdgeInsets.all(8),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contact number',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '';
+                  }
+                  return null;
+                },
+                onChanged: (text) {
+                  _setcontactno(text);
+                },
+                keyboardType: TextInputType.number,
+              )),
+          Padding(
+              padding: EdgeInsets.all(8),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '';
+                  }
+                  return null;
+                },
+                onChanged: (text) {
+                  _setemail(text);
+                },
+                keyboardType: TextInputType.number,
+              )),
+          Padding(
+              padding: EdgeInsets.all(8),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Subject offered',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '';
+                  }
+                  return null;
+                },
+                onChanged: (text) {
+                  _setsubject(text);
+                },
+                keyboardType: TextInputType.number,
+              )),
+          Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _create();
+                  }
+                },
+                child: const Text('Submit'),
+              )),
+        ],
+      ),
+    );
+  }
 }
 
