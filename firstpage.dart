f9bded983a71b319037fe045bfe7774f8a9c2338
AisyahSyahirah <92360106+AisyahSyahirah@users.noreply.github.com>
2022-07-01 03:17:41 +0800

    Update firstpage.dart

--- firstpage.dart
+++ firstpage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:tutorsystem/secpage.dart';
+import 'package:tutor/secpage.dart';
 import 'routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -11,66 +12,41 @@ class FirstPage extends StatefulWidget {
 
 class _FirstPageState extends State<FirstPage> {
   final _formKey = GlobalKey<FormState>();
+  FirebaseAuth auth = FirebaseAuth.instance;
+ 
+var email, password;
 
-  void _adminlogin() {
-    _showDialog();
+void _setEmail(String text){
+  setState(() {
+    email=text;
+    
+  });
+}
+
+void _setPass(String text){
+  setState(() {
+    password=text;
+    
+  });
+}
+
+void login ()async{
+  try {
+  UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+    email: email,
+    password:password
+  );
+  Navigator.pushNamed(context, Routes.secPage);
+
+} 
+  on FirebaseAuthException catch (e) {
+  if (e.code == 'user-not-found') {
+    print('No user found for that email.');
+  } else if (e.code == 'wrong-password') {
+    print('Wrong password provided for that user.');
   }
-
-  void _showDialog() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: new Text("Login for Admin", textAlign: TextAlign.center),
-            actions: <Widget>[
-              Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        //controller: weightController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Email',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                        },
-                      ),
-                      TextFormField(
-                        //controller: weightController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Password',
-                        ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                        },
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: ElevatedButton(
-                            child: const Text('Submit'),
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                Navigator.pushNamed(
-                                  context,
-                                  Routes.secPage,
-                                );
-                              }
-                            }),
-                      )
-                    ],
-                  ))
-            ],
-          );
-        });
-  }
-
+}
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,28 +59,65 @@ class _FirstPageState extends State<FirstPage> {
         child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Container(
-                child: Text('APPLICATION NAME'),
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, color: Colors.purple),
-                padding: EdgeInsets.all(70.0),
-                margin: EdgeInsets.all(15.0),
-              ),
-              Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: ElevatedButton(
-                      child: const Text('Continue as guest'),
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          Routes.secPage,
-                        );
-                      })),
-              Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: ElevatedButton(
-                      child: const Text('Login for admin'),
-                      onPressed: _adminlogin)),
+              new Text('APPLICATION NAME'),
+              new Text('Login for Admin'),
+                  Form(
+            key: _formKey,
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child:TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'email'
+                    ),
+                    validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  }, 
+                    onChanged: (text) {
+                      _setEmail(text);
+                    },
+                  )
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child:TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password'
+                    ),
+                    validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  }, 
+                    onChanged: (text) {
+                      _setPass(text);
+                    },
+                  )
+                ),
+
+                Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ElevatedButton(
+                    onPressed: (){
+                      if (_formKey.currentState!.validate()) {
+                        login();
+                      }
+                    },
+                    child: const Text('Login'),
+                  )
+                ),
+                   
+                  ],
+                ),
+              )
             ]),
       )),
     );
