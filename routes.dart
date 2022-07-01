89779c42ca3d89d2409923882ff1fa0412cf343c
yasminhana <92916680+yasminhana@users.noreply.github.com>
2022-07-01 14:30:36 +0800

    Delete routes.dart

--- routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tutorsystem/firstpage.dart';
-
-class Routes {
-  static const firstPage = '/firstpage';
-  static const secPage = '/secpage';
-  static const thirdPage = '/thirdpage';
-  static const fourthPage = '/fourthpage';
-}
